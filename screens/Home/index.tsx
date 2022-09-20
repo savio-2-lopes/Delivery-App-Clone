04fccdfcb2c7ca33ebae502aef7b4eb34174f0c3
savio-2-lopes <savioaugulopes@gmail.com>
2022-09-19 23:39:02 -0300

    Update JS to TSX

---
+++ screens/Home/index.tsx
@@ -0,0 +1,66 @@
+import React, { useLayoutEffect } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import {
+  CaretDown,
+  Faders,
+  MagnifyingGlass,
+  User,
+} from "phosphor-react-native";
+
+const Home = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView className="bg-white pt-8">
+      <Text className="text-red-500">
+        {/* Header */}
+        <View className="flex flex-row pb-3 items-center mx-4 space-x-2 px-4">
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
+            <Text className="font-bold text-xl">
+              Current Location <CaretDown size={20} color="#00CCBB" />
+            </Text>
+          </View>
+
+          <User size={35} color="#00CCBB" />
+        </View>
+
+        {/* Search */}
+        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+            <MagnifyingGlass color="gray" size={20} />
+            <TextInput
+              placeholder="Restaurants and cuisines"
+              keyboardType="default"
+            />
+          </View>
+
+          <Faders color="#00CCbb" />
+        </View>
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
